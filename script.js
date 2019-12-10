f439690b5e765b1fd6ed82f24cb04bda6b326bfb
Andrey <tynyanyi@mail.ru>
2019-12-10 18:04:29 +0300

    second +

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let money = 0, time  = 0, one_day = 0;
+let money = +prompt("Ваш бюджет на месяц?: ", ''),
+    time  = prompt("Введите дату в формате YYYY-MM-DD", ''), 
+    one_day = 0;
 
 /*Создаем обьект*/
 let appData = {
@@ -14,22 +16,27 @@ while(confirm("Добавить расход? ")){
     /**
      * Спрашиваем пользователя
      */
-    let quest1 = prompt("Обязательная статья расхода в этом месяце?");
-    let quest2 = prompt("Во сколько обойдется?");
+    let a = prompt("Обязательная статья расхода в этом месяце?");
+    let b = parseFloat(prompt("Во сколько обойдется?"));
 
-
-    if(quest2) money = quest2; // если есть значение заполняем
-    //заполняем вопрос/ответ
-    appData.expenses[quest1] = quest2;
+    if(typeof(a) === 'string' && a.length > 0 && b.length > 0 &&  a.length < 50){
+        //заполняем вопрос/ответ
+        appData.expenses[a] = b;
+    }else{
+        alert("Не правильно указанны данные");
+    }
 }
 
 
 
 //перебираем все статьи расхода сумируем расход
-for (var i in appData.expenses){
+for (let i in appData.expenses){
     one_day += parseFloat(appData.expenses[i]);
 }
 
-alert("your bujet for one day: " + one_day / 30);
+appData.moneyPreDay = appData.bujet / 30;
+
+alert("your bujet for one day: " +  appData.moneyPreDay);
+
 
 console.log(appData);
