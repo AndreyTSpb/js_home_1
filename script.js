7380d313599d495c6915bfc35ab6813c87b2e9aa
Andrey <tynyanyi@mail.ru>
2019-12-12 14:47:04 +0300

    tree lesson

--- script.js
+++ script.js
@@ -2,41 +2,90 @@ let money = +prompt("Ваш бюджет на месяц?: ", ''),
     time  = prompt("Введите дату в формате YYYY-MM-DD", ''), 
     one_day = 0;
 
+
 /*Создаем обьект*/
 let appData = {
-    bujet : money,
+    bujet : {},
     timeData : time,
     expenses : {},
     optionalExpenses : {},
     income : [],
-    savings : false
+    savings : true,
+    monthIncome: 0
 };
 
-while(confirm("Добавить расход? ")){
-    /**
-     * Спрашиваем пользователя
-     */
-    let a = prompt("Обязательная статья расхода в этом месяце?");
-    let b = parseFloat(prompt("Во сколько обойдется?"));
+function detectDayBugget(money){
+    appData.bujet       = money;
+    appData.moneyPreDay = money / 30;
+    alert("your bujet for one day: " +  appData.moneyPreDay);
+}
 
-    if(typeof(a) === 'string' && a.length > 0 && b.length > 0 &&  a.length < 50){
-        //заполняем вопрос/ответ
-        appData.expenses[a] = b;
+function detectLevel(){
+    if(appData.moneyPreDay < 50 ){
+        return "you loser";
+    }else if (appData.moneyPreDay > 100){
+        return "you win";
     }else{
-        alert("Не правильно указанны данные");
+        return "comdi comsa";
     }
 }
 
-
-
-//перебираем все статьи расхода сумируем расход
-for (let i in appData.expenses){
-    one_day += parseFloat(appData.expenses[i]);
+function chooseOptExpenses(){
+    for (let i = 1; i<4; i++){
+        appData.optionalExpenses[i] = prompt("Не обязательные расходы?");
+    }
 }
 
-appData.moneyPreDay = appData.bujet / 30;
+detectDayBugget(money);
+alert(detectLevel());
 
-alert("your bujet for one day: " +  appData.moneyPreDay);
+function addExpenses(){
+    while(confirm("Добавить расход? ")){
+        /**
+         * Спрашиваем пользователя
+         */
+        let a = prompt("Обязательная статья расхода в этом месяце?");
+        let b = prompt("Во сколько обойдется?");
+    
+        if(a.length > 0 && b.length > 0){
+            //заполняем вопрос/ответ
+            appData.expenses[a] = +b;
+        }else{
+            alert("Не правильно указанны данные");
+        }
+    }
+}
+
+function checkSavings(){
+    if(appData.savings == true){
+        let save = +prompt("Какова сумма накопления?"), 
+            procent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/1000/12*procent;
+        alert("Доход в месяц : " + appData.monthIncome);
+    }
+}
+
+function summExpenses(){
+    //перебираем все статьи расхода сумируем расход
+    for (let i in appData.expenses){
+        one_day += parseFloat(appData.expenses[i]);
+    }
+}
+
+ if(confirm("Добавить не обязательную статью расходов?")){
+     chooseOptExpenses();
+ }
+
+ if(confirm("Добавить обязательные рассходы?")){
+     addExpenses();
+ }
+
+ if(appData.expenses){
+     summExpenses();
+ }
+
+ checkSavings();
 
 
 console.log(appData);
