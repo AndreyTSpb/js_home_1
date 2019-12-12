61c3f4ae38070ca1a430dad56af931ddf8f1ba52
Andrey <tynyanyi@mail.ru>
2019-12-12 16:52:52 +0300

    lesson 4 -up date

--- script.js
+++ script.js
@@ -10,7 +10,88 @@ let appData = {
     optionalExpenses : {},
     income : [],
     savings : true,
-    monthIncome: 0
+    monthIncome: 0,
+    detectDayBugget : function(money){
+        appData.bujet       = money;
+        appData.moneyPreDay = money / 30;
+        alert("your bujet for one day: " +  appData.moneyPreDay);
+    },
+    detectLevel : function(){
+        if(appData.moneyPreDay < 50 ){
+            return "you loser";
+        }else if (appData.moneyPreDay > 100){
+            return "you win";
+        }else{
+            return "comdi comsa";
+        }
+    },
+    chooseOptExpenses : function(){
+        for (let i = 1; i<4; i++){
+            appData.optionalExpenses[i] = prompt("Не обязательные расходы?", '');
+        }
+    },
+    chooseIncome : function(){
+        let ans = prompt("Укажитедополнительные доходы (разделитель запятая)",'');
+        if(!isNaN(ans) || ans == null || ans == ""){
+            ans = prompt("Укажитедополнительные доходы (разделитель запятая)",'');
+        }
+        appData.income = ans.split(', ');
+    },
+    addExpenses : function(){
+        while(confirm("Добавить расход? ")){
+            /**
+             * Спрашиваем пользователя
+             */
+            let a = prompt("Обязательная статья расхода в этом месяце?", '');
+            let b = prompt("Во сколько обойдется?", '');
+        
+            if(a.length > 0 && b.length > 0){
+                //заполняем вопрос/ответ
+                appData.expenses[a] = +b;
+            }else{
+                alert("Не правильно указанны данные");
+            }
+        }
+        //перебираем все статьи расхода сумируем расход
+        for (let i in appData.expenses){
+            one_day += parseFloat(appData.expenses[i]);
+        }
+    },
+    checkSavings : function(){
+        if(appData.savings == true){
+            let save = +prompt("Какова сумма накопления?"), 
+                procent = +prompt("Под какой процент?");
+    
+            appData.monthIncome = save/1000/12*procent;
+            alert("Доход в месяц : " + appData.monthIncome);
+        }
+    },
+    summExpenses : function(){
+        //перебираем все статьи расхода сумируем расход
+        for (let i in appData.expenses){
+            one_day += parseFloat(appData.expenses[i]);
+        }
+    },
+    viewData : function(){
+        let str = "Our programm has in data: \n";
+
+        for (let key in appData){
+            if(typeof(appData[key]) === 'function'){
+                //fff
+            }else{
+                if(appData[key] !== null && typeof appData[key] === 'object'){
+                    str += key+ " : \n";
+                    for(let key2 in appData[key]){
+                        str += "    " + key2 + " : " + appData[key][key2] + "\n"; 
+                    }
+                }else{
+                str += key + " : " + appData[key] + '\n';
+                }
+            }
+       
+        }
+        return str;
+    }
 };
 
 function start(){
@@ -24,79 +105,20 @@ function start(){
 }
 start();
 
-function detectDayBugget(money){
-    appData.bujet       = money;
-    appData.moneyPreDay = money / 30;
-    alert("your bujet for one day: " +  appData.moneyPreDay);
-}
+appData.chooseIncome();
+appData.detectDayBugget(money);
+//chooseIncome();
+//detectDayBugget(money);
+alert(appData.detectLevel());
 
-function detectLevel(){
-    if(appData.moneyPreDay < 50 ){
-        return "you loser";
-    }else if (appData.moneyPreDay > 100){
-        return "you win";
-    }else{
-        return "comdi comsa";
-    }
-}
 
-function chooseOptExpenses(){
-    for (let i = 1; i<4; i++){
-        appData.optionalExpenses[i] = prompt("Не обязательные расходы?");
-    }
-}
-
-function chooseIncome(){
-    let ans = prompt("Укажитедополнительные доходы (разделитель запятая)",'');
-    if(!isNaN(ans) || ans == null || ans == ""){
-        ans = prompt("Укажитедополнительные доходы (разделитель запятая)",'');
-    }
-     appData.income = ans.split(', ');
-}
-chooseIncome();
-detectDayBugget(money);
-alert(detectLevel());
-
-function addExpenses(){
-    while(confirm("Добавить расход? ")){
-        /**
-         * Спрашиваем пользователя
-         */
-        let a = prompt("Обязательная статья расхода в этом месяце?");
-        let b = prompt("Во сколько обойдется?");
-    
-        if(a.length > 0 && b.length > 0){
-            //заполняем вопрос/ответ
-            appData.expenses[a] = +b;
-        }else{
-            alert("Не правильно указанны данные");
-        }
-    }
-}
-
-function checkSavings(){
-    if(appData.savings == true){
-        let save = +prompt("Какова сумма накопления?"), 
-            procent = +prompt("Под какой процент?");
-
-        appData.monthIncome = save/1000/12*procent;
-        alert("Доход в месяц : " + appData.monthIncome);
-    }
-}
-
-function summExpenses(){
-    //перебираем все статьи расхода сумируем расход
-    for (let i in appData.expenses){
-        one_day += parseFloat(appData.expenses[i]);
-    }
-}
 
  if(confirm("Добавить не обязательную статью расходов?")){
-     chooseOptExpenses();
+     appData.chooseOptExpenses();
  }
 
  if(confirm("Добавить обязательные рассходы?")){
-     addExpenses();
+     appData.addExpenses();
  }
 
  if(appData.income.length > 0){
@@ -109,26 +131,9 @@ function summExpenses(){
  }
 
  if(appData.expenses){
-     summExpenses();
+     appData.summExpenses();
  }
 
- checkSavings();
+ appData.checkSavings();
 
- function viewData(){
-     let str = "Our programm has in data: \n";
-
-     for (let key in appData){
-         if(appData[key] !== null && typeof appData[key] === 'object'){
-             str += key+ " : \n";
-             for(let key2 in appData[key]){
-                 str += "    " + key2 + " : " + appData[key][key2] + "\n"; 
-             }
-         }else{
-            str += key + " : " + appData[key] + '\n';
-         }
-    
-     }
-     return str;
- }
-
-console.log(viewData());
+console.log(appData.viewData());
